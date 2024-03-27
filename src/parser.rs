5674f77bfce6971233cee53675bb39e64fbf1f6a
magokstad <magokstad@gmail.com>
2024-03-27 04:54:19 +0100

    fix: slight fixes and changes, stmt parser borked

--- src/parser.rs
+++ src/parser.rs
@@ -56,7 +56,8 @@ pub enum Stmt {
         cond: Box<Expr>,
         body: Vec<Stmt>,
     },
-    ExprStmt(Vec<Expr>),
+    ExprStmt(Box<Expr>),
+    StmtList(Vec<Stmt>),
     VarDecl {
         name: String,
         typ: Option<Type>,
@@ -81,6 +82,9 @@ pub enum Expr {
     Neg(Box<Expr>),
     Ref(Box<Expr>),
     Deref(Box<Expr>),
+
+    Inc(Box<Expr>),
+    Dec(Box<Expr>),
 
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
@@ -199,7 +203,6 @@ pub fn stmt_parser<'a, I>() -> impl Parser<'a, I, Stmt, extra::Err<Rich<'a, Toke
 where
     I: ValueInput<'a, Token = Token<'a>, Span = SimpleSpan>,
 {
-    println!("expr outer");
     let literal = select! {
         Token::IntLit(s) => Expr::IntLit(s),
         Token::FloatLit(s) => Expr::FloatLit(s),
@@ -207,6 +210,11 @@ where
         Token::CharLit(s) => Expr::CharLit(s)
     }
     .labelled("literal");
+
+    let inc_dec = select! {
+        Token::Increment => Expr::Inc as UnaryOpFn,
+        Token::Decrement => Expr::Dec as UnaryOpFn,
+    };
 
     let unary_op = select! {
         Token::Minus => Expr::Neg as UnaryOpFn,
@@ -239,20 +247,18 @@ where
     let comp_weak = select! {
         Token::EqualEqual => Expr::Eq as BinaryOpFn,
         Token::NotEqual => Expr::Neq as BinaryOpFn,
-    };
+    }
+    .labelled("comp_weak");
 
     let assign = select! {
         Token::Equal => Expr::Assign as BinaryOpFn
-    };
+    }
+    .labelled("assign");
 
     let var = select! {Token::Name(n) => Expr::Var(n)}.labelled("var");
     let name = select! {Token::Name(n) => n}.labelled("name");
     let null = select! {Token::Null => Expr::Null}.labelled("null");
 
-    // C operator precedence:
-    // https://en.cppreference.com/w/c/language/operator_precedence
-
-    // TODO: With new parsers, this might be easier to do some other way w/o replace_parse_temp
     let mut stmt = Recursive::declare();
 
     let expr = recursive(|expr| {
@@ -289,30 +295,33 @@ where
             name: rhs,
         });
 
+        let idec = inc_dec.map(|op| op(Box::new(Expr::ParserNone)));
+
+        // TODO: With new parsers, this might be easier to do some other way w/o replace_parse_temp
         let first = atom.clone().foldl(
-            choice((member_access, func_call, subscription)).repeated(),
+            choice((member_access, func_call, subscription, idec)).repeated(),
             |exp, firs| firs.replace_parse_temp(exp),
         );
 
-        // let iff = just(Token::If)
-        //     .ignore_then(expr.clone())
-        //     .then(stmt_list.clone())
-        //     .then(
-        //         just(Token::Else)
-        //             .ignore_then(just(Token::If))
-        //             .ignore_then(expr.clone())
-        //             .then(stmt_list.clone())
-        //             .repeated()
-        //             .collect::<Vec<_>>(),
-        //     )
-        //     .then(just(Token::Else).ignore_then(stmt_list.clone()).or_not())
-        //     .map(|(((exp, l1), l2a), ol3)| Expr::If {
-        //         iff: (Box::new(exp), l1),
-        //         elseifs: l2a.into_iter().map(|(e, vs)| (Box::new(e), vs)).collect(),
-        //         elsee: ol3.unwrap_or(vec![]),
-        //     });
+        let iff = just(Token::If)
+            .ignore_then(expr.clone())
+            .then(stmt_list.clone())
+            .then(
+                just(Token::Else)
+                    .ignore_then(just(Token::If))
+                    .ignore_then(expr.clone())
+                    .then(stmt_list.clone())
+                    .repeated()
+                    .collect::<Vec<_>>(),
+            )
+            .then(just(Token::Else).ignore_then(stmt_list.clone()).or_not())
+            .map(|(((exp, l1), l2a), ol3)| Expr::If {
+                iff: (Box::new(exp), l1),
+                elseifs: l2a.into_iter().map(|(e, vs)| (Box::new(e), vs)).collect(),
+                elsee: ol3.unwrap_or(vec![]),
+            });
 
-        let end = first.pratt((
+        let end = first.or(iff).pratt((
             prefix(8, unary_op, |op: UnaryOpFn, rhs| op(Box::new(rhs))),
             infix(left(7), bin_op_strong, |lhs, op: BinaryOpFn, rhs| {
                 op(Box::new(lhs), Box::new(rhs))
@@ -332,53 +341,75 @@ where
         ));
 
         end
-    });
+    })
+    .labelled("expr")
+    .as_context();
 
-    // let name = select! {Token::Name(n) => n};
     stmt.define({
         let expr_stmt = expr
             .clone()
-            .separated_by(just(Token::Comma))
-            .collect::<Vec<_>>()
-            .map(|e| Stmt::ExprStmt(e));
+            // .then_ignore(just(Token::SemiColon))
+            .map(|e| Stmt::ExprStmt(Box::new(e)))
+            .labelled("expr_stmt")
+            .as_context();
 
         let var_decl = name
             .then(just(Token::Colon).ignore_then(type_parser()).or_not())
             .then_ignore(just(Token::ColEqual))
             .then(expr.clone())
-            .map(|((name, typ), expr)| Stmt::VarDecl { name, typ, expr });
+            // .then_ignore(just(Token::SemiColon))
+            .map(|((name, typ), expr)| Stmt::VarDecl { name, typ, expr })
+            .labelled("var_decl")
+            .as_context();
+
+        // WARN: COULD BE INFINITE???
+        let decl_list = var_decl
+            .clone()
+            .separated_by(just(Token::Comma))
+            .collect::<Vec<_>>()
+            .map(Stmt::StmtList)
+            .labelled("decl_list")
+            .as_context();
+
+        let stmt_list = stmt
+            .clone()
+            .separated_by(just(Token::Comma))
+            .collect::<Vec<_>>()
+            .map(Stmt::StmtList)
+            .labelled("stmt_list")
+            .as_context();
 
         let for_stmt = just(Token::For)
-            .ignore_then(expr_stmt.clone())
+            .ignore_then(decl_list.clone())
             .then_ignore(just(Token::SemiColon))
             .then(expr.clone())
             .then_ignore(just(Token::SemiColon))
-            .then(expr_stmt.clone())
+            .then(stmt_list.clone())
             .then(
                 stmt.clone()
-                    .separated_by(just(Token::SemiColon))
-                    .collect::<Vec<_>>(),
+                    .then_ignore(just(Token::SemiColon))
+                    .repeated()
+                    // .separated_by(just(Token::SemiColon))
+                    // .allow_trailing()
+                    .collect::<Vec<_>>()
+                    .delimited_by(just(Token::LeftBrace), just(Token::RightBrace)),
             )
             .map(|(((i, c), a), b)| Stmt::ForStmt {
                 init: Box::new(i),
                 cond: Box::new(c),
                 after: Box::new(a),
                 body: b,
-            });
+            })
+            .labelled("for_stmt")
+            .as_context();
 
-        var_decl.or(expr_stmt).or(for_stmt)
+        for_stmt.or(var_decl).or(expr_stmt)
+        // .recover_with(var_decl)
+        // choice((for_stmt, expr_stmt, var_decl))
     });
 
     stmt
 }
-
-// pub fn stmt_parser<'a, I>() -> impl Parser<'a, I, Stmt, extra::Err<Rich<'a, Token<'a>>>> + Clone
-// where
-//     I: ValueInput<'a, Token = Token<'a>, Span = SimpleSpan>,
-// {
-
-//     stmt
-// }
 
 pub fn parser<'a, I>() -> impl Parser<'a, I, File, extra::Err<Rich<'a, Token<'a>>>> + Clone
 where
@@ -411,7 +442,9 @@ where
             args,
             ret,
             body,
-        });
+        })
+        .labelled("fn_def")
+        .as_context();
 
     // Temporary
     fn_def.map(|f| File {
@@ -427,261 +460,3 @@ where
         ],
     })
 }
-
-// pub fn parser<'a, I>() -> impl Parser<'a, I, File, extra::Err<Rich<'a, Token<'a>>>>
-// where
-//     I: ValueInput<'a, Token = Token<'a>, Span = SimpleSpan>,
-// {
-//     // pub fn parser() -> impl Parser<Token, File, Error = Simple<Token>> {
-//     let literal = select! {
-//         Token::IntLit(s) => Expr::IntLit(s),
-//         Token::FloatLit(s) => Expr::FloatLit(s),
-//         Token::StringLit(s) => Expr::StringLit(s),
-//         Token::CharLit(s) => Expr::CharLit(s)
-//     };
-
-//     let unary_op = select! {
-//         Token::Minus => Expr::Neg as UnaryOpFn,
-//         Token::Ampersand => Expr::Ref as UnaryOpFn,
-//         Token::Asterisk => Expr::Deref as UnaryOpFn,
-//     };
-
-//     let bin_op_strong = select! {
-//         Token::Asterisk => Expr::Mult as BinaryOpFn,
-//         Token::Slash => Expr::Div as BinaryOpFn,
-//         Token::Percent => Expr::Mod as BinaryOpFn,
-//     };
-
-//     let bin_op_weak = select! {
-//         Token::Plus => Expr::Add as BinaryOpFn,
-//         Token::Minus => Expr::Sub as BinaryOpFn,
-//     };
-
-//     let comp_strong = select! {
-//         Token::LessEqual => Expr::Leq as BinaryOpFn,
-//         Token::Less => Expr::Lt as BinaryOpFn,
-//         Token::GreaterEqual => Expr::Geq as BinaryOpFn,
-//         Token::Greater => Expr::Gt as BinaryOpFn,
-//     };
-
-//     // let comp_weak = select! {
-//     //     Token::EqualEqual => Expr::Eq as BinaryOpFn,
-//     //     Token::NotEqual => Expr::Neq as BinaryOpFn,
-//     // };
-
-//     // let assign = select! {
-//     //     Token::Equal => Expr::Assign as BinaryOpFn
-//     // };
-
-//     let var = select! {Token::Name(n) => Expr::Var(n)};
-//     let name = select! {Token::Name(n) => n};
-//     let null = select! {Token::Null => Expr::Null};
-
-//     let base_type = select! {
-//         Token::Int => Type::Int,
-//         Token::Float => Type::Float,
-//         Token::Char => Type::Char,
-//         Token::Void => Type::Void,
-//         Token::Name(s) => Type::Custom(s)
-//     };
-
-//     let ref_type = select! {
-//         Token::Ampersand => Type::Pointer as UnaryTypeFn,
-//         // Token::Asterisk => Type::Pointer as UnaryTypeFn
-//     };
-
-//     // let typ = recursive(|_| {
-//     let refer = ref_type
-//         .repeated()
-//         .foldr(base_type, |op, rhs| op(Box::new(rhs)));
-
-//     let array = refer.foldl(
-//         just(Token::LeftBracket)
-//             .ignore_then(just(Token::RightBracket))
-//             .repeated(),
-//         |typ, _| Type::Array(Box::new(typ)),
-//     );
-//     // array
-//     // });
-
-//     // remove if changing typ to include more types
-//     let typ = array;
-
-//     // Hack to get expr in this scope
-//     let mut expr_maybe = None;
-//     let stmt = recursive(|stmt| {
-//         let expr = recursive(|expr| {
-//             let inner = expr
-//                 .clone()
-//                 .delimited_by(just(Token::LeftParen), just(Token::RightParen));
-
-//             let ret = just(Token::Ret).ignore_then(expr.clone());
-
-//             let atom = literal.or(var).or(null).or(ret).or(inner);
-
-//             // let stmt_list = stmt
-//             //     .clone()
-//             //     .separated_by(just(Token::SemiColon))
-//             //     .collect::<Vec<_>>()
-//             //     .delimited_by(just(Token::LeftBrace), just(Token::RightBrace));
-
-//             let subscription = expr
-//                 .clone()
-//                 .delimited_by(just(Token::LeftBracket), just(Token::RightBracket))
-//                 .map(|r| Expr::Index(Box::new(Expr::ParserNone), Box::new(r)));
-
-//             let func_call = expr
-//                 .clone()
-//                 .separated_by(just(Token::Comma))
-//                 .collect::<Vec<_>>()
-//                 .delimited_by(just(Token::LeftParen), just(Token::RightParen))
-//                 .map(|rs| Expr::FnCall {
-//                     expr: Box::new(Expr::ParserNone),
-//                     args: rs.into_iter().map(|x| Box::new(x)).collect(),
-//                 });
-
-//             let member_access = just(Token::Dot).ignore_then(name).map(|rhs| Expr::Member {
-//                 expr: Box::new(Expr::ParserNone),
-//                 name: rhs,
-//             });
-
-//             // C operator precedence:
-//             // https://en.cppreference.com/w/c/language/operator_precedence
-
-//             // TODO: With new parsers, this might be easier to do some other way w/o replace_parse_temp
-//             let first = atom.clone().foldl(
-//                 member_access.or(func_call).or(subscription).repeated(),
-//                 |exp, firs| firs.replace_parse_temp(exp),
-//             );
-
-//             let second = unary_op
-//                 .repeated()
-//                 .foldr(first.clone(), |op, rhs| op(Box::new(rhs)));
-
-//             let third = second.clone().foldl(
-//                 bin_op_strong.then(second.clone()).repeated(),
-//                 |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-//             );
-
-//             let fourth = third.clone().foldl(
-//                 bin_op_weak.then(third.clone()).repeated(),
-//                 |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-//             );
-
-//             // skipping fifth for now
-//             let fifth = fourth;
-
-//             let sixth = fifth.clone().foldl(
-//                 comp_strong.then(fifth.clone()).repeated(),
-//                 |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-//             );
-
-//             // let seventh = sixth.clone().foldl(
-//             //     comp_weak.then(sixth.clone()).repeated(),
-//             //     |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-//             // );
-
-//             // // skipping to thirteenth
-//             // let thirteenth = seventh.clone();
-//             // let fourteenth = thirteenth.clone().foldl(
-//             //     assign.then(thirteenth.clone()).repeated(),
-//             //     |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-//             // );
-
-//             // let iff = just(Token::If)
-//             //     .ignore_then(expr.clone())
-//             //     .then(stmt_list.clone())
-//             //     .then(
-//             //         just(Token::Else)
-//             //             .ignore_then(just(Token::If))
-//             //             .ignore_then(expr.clone())
-//             //             .then(stmt_list.clone())
-//             //             .repeated()
-//             //             .collect::<Vec<_>>(),
-//             //     )
-//             //     .then(just(Token::Else).ignore_then(stmt_list.clone()).or_not())
-//             //     .map(|(((exp, l1), l2a), ol3)| Expr::If {
-//             //         iff: (Box::new(exp), l1),
-//             //         elseifs: l2a.into_iter().map(|(e, vs)| (Box::new(e), vs)).collect(),
-//             //         elsee: ol3.unwrap_or(vec![]),
-//             //     });
-
-//             // fourteenth //.or(iff)
-//             sixth
-//         });
-//         // Hack to get expr in outer scope
-//         expr_maybe = Some(expr.clone());
-
-//         let expr_stmt = expr
-//             .clone()
-//             .separated_by(just(Token::Comma))
-//             .collect::<Vec<_>>()
-//             .map(|e| Stmt::ExprStmt(e));
-
-//         let var_decl = name
-//             .then(just(Token::Colon).ignore_then(typ.clone()).or_not())
-//             .then_ignore(just(Token::ColEqual))
-//             .then(expr.clone())
-//             .map(|((name, typ), expr)| Stmt::VarDecl { name, typ, expr });
-
-//         // let for_stmt = just(Token::For)
-//         //     .ignore_then(expr_stmt.clone())
-//         //     .then_ignore(just(Token::SemiColon))
-//         //     .then(expr.clone())
-//         //     .then_ignore(just(Token::SemiColon))
-//         //     .then(expr_stmt.clone())
-//         //     .then(
-//         //         stmt.clone()
-//         //             .separated_by(just(Token::SemiColon))
-//         //             .collect::<Vec<_>>(),
-//         //     )
-//         //     .map(|(((i, c), a), b)| Stmt::ForStmt {
-//         //         init: Box::new(i),
-//         //         cond: Box::new(c),
-//         //         after: Box::new(a),
-//         //         body: b,
-//         //     });
-
-//         var_decl.or(expr_stmt)
-//         // .or(for_stmt)
-//     });
-//     // Hack to get expr in this scope
-//     let expr = expr_maybe.unwrap();
-
-//     // let name_type_list = name
-//     //     .then_ignore(just(Token::Colon))
-//     //     .then(typ.clone())
-//     //     .separated_by(just(Token::Comma))
-//     //     .collect::<Vec<_>>();
-
-//     // let fn_def = just(Token::Fn)
-//     //     .ignore_then(name)
-//     //     .then(
-//     //         name_type_list
-//     //             .clone()
-//     //             .delimited_by(just(Token::LeftParen), just(Token::RightParen)),
-//     //     )
-//     //     .then(typ.clone())
-//     //     .then(
-//     //         stmt.clone()
-//     //             .separated_by(just(Token::SemiColon))
-//     //             .collect::<Vec<_>>()
-//     //             .delimited_by(just(Token::LeftBrace), just(Token::RightBrace)),
-//     //     )
-//     //     .map(|(((name, args), ret), body)| OuterStmt::FnDef {
-//     //         name,
-//     //         args,
-//     //         ret,
-//     //         body,
-//     //     });
-
-//     // Temporary
-//     expr.map(|f| File {
-//         includes: vec![],
-//         stmts: vec![OuterStmt::GlobalVarDecl {
-//             name: "asd".to_string(),
-//             typ: None,
-//             expr: f,
-//         }],
-//     })
-// }
